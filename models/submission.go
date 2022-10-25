7a4f2639da4dca56b98ff3fff898adf5c82a051f
Minh Phạm Vũ <minh.pv@teko.vn>
2022-10-25 11:02:12 +0700

    Add assignment, class, submission models

--- models/submission.go
+++ models/submission.go
@@ -1,4 +1,12 @@
 package models
 
+import "gorm.io/gorm"
+
 type Submission struct {
+	gorm.Model
+	AssignmentID int
+	StudentID    int
+	Content      string
+	Grade        float32
+	Attachment   []Attachment `gorm:"foreignKey:SubmissionID"`
 }
