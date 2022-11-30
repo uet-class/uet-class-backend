adf01a3f3aa3cdaf9ab323c4e4777039675bce86
thainm <thainm.uet@gmail.com>
2022-11-30 07:11:56 +0700

    Update storage client connection

--- storage/gcs.go
+++ storage/gcs.go
@@ -2,11 +2,9 @@ package storage
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	"cloud.google.com/go/storage"
 	"github.com/uet-class/uet-class-backend/config"
@@ -24,23 +22,6 @@ func InitStorageClient() {
 	if err != nil {
 		log.Fatal(http.StatusInternalServerError, err)
 	}
-	println("It is not dying...", storageClient.Buckets(ctx, "uet-class"))
-	defer storageClient.Close()
-
-	// Sets the name for the new bucket.
-	bucketName := "my-new-bucket"
-
-	// Creates a Bucket instance.
-	bucket := storageClient.Bucket(bucketName)
-
-	// Creates the new bucket.
-	ctx, cancel := context.WithTimeout(ctx, time.Second*10)
-	defer cancel()
-	if err := bucket.Create(ctx, config.GetConfig().GetString("GOOGLE_PROJECT_ID"), nil); err != nil {
-		log.Fatalf("Failed to create bucket: %v", err)
-	}
-
-	fmt.Printf("Bucket %v created.\n", bucketName)
 }
 
 func GetStorageClient() *storage.Client {
