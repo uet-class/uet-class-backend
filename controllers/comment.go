066344129785afe1c782d0cfeb16eb9b3233bef2
thainm <thainm.uet@gmail.com>
2022-12-04 05:34:23 +0700

    Add APIs: CRUD Comment (by not working yet)

---
+++ controllers/comment.go
@@ -0,0 +1,79 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/uet-class/uet-class-backend/database"
+	"github.com/uet-class/uet-class-backend/models"
+)
+
+type CommentController struct{}
+
+func getCommentFromId(postId string) (*models.Post, error) {
+	db := database.GetDatabase()
+
+	var matchedPost *models.Post
+	if err := db.First(&matchedPost, postId).Error; err != nil {
+		return nil, err
+	}
+	return matchedPost, nil
+}
+
+func (comment CommentController) CreateComment(c *gin.Context) {
+	db := database.GetDatabase()
+
+	var reqComment models.Comment
+	if err := c.BindJSON(&reqComment); err != nil {
+		ResponseHandler(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	if err := db.Create(&reqComment).Error; err != nil {
+		ResponseHandler(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	ResponseHandler(c, http.StatusOK, "Succeed")
+}
+
+func (comment CommentController) UpdateComment(c *gin.Context) {
+	db := database.GetDatabase()
+
+	var updatedComment models.Comment
+	if err := c.BindJSON(&updatedComment); err != nil {
+		ResponseHandler(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	matchedComment, err := getCommentFromId(c.Param("id"))
+	if err != nil {
+		ResponseHandler(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	if infoIsChanged(updatedComment.Content, matchedComment.Content) {
+		matchedComment.Content = updatedComment.Content
+	}
+
+	if err := db.Save(&matchedComment).Error; err != nil {
+		ResponseHandler(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	ResponseHandler(c, http.StatusOK, "Succeed")
+}
+
+func (comment CommentController) DeleteComment(c *gin.Context) {
+	db := database.GetDatabase()
+
+	matchedComment, err := getCommentFromId(c.Param("id"))
+	if err != nil {
+		ResponseHandler(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	if err := db.Delete(&matchedComment).Error; err != nil {
+		ResponseHandler(c, http.StatusInternalServerError, err)
+		return
+	}
+	ResponseHandler(c, http.StatusOK, "Succeed")
+}
