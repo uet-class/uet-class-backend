bd55267228da0ff3829b4243b7944a713abbc4d4
Nguyen Minh Thai <54463108+thai-nm@users.noreply.github.com>
2022-12-05 21:10:13 +0700

    Update mechanism: Exporting environment vars (#51)
    * Update environment var mechanism
    
    * Update Dockerfile and optimize CICD pipeline

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM ubuntu:20.04
-
+FROM golang:1.19 AS build
 WORKDIR /app
+COPY . /app
+RUN go build /app/main.go
 
-COPY . /app/
-
-RUN ls -la /app
-
-CMD [ "./uet-class-backend" ]
+FROM ubuntu:20.04 AS deploy
+WORKDIR /app
+COPY --from=build /app/main /app/
+CMD [ "/app/main" ]
--- database/redis.go
+++ database/redis.go
@@ -3,22 +3,23 @@ package database
 import (
 	"context"
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/go-redis/redis/v8"
-	"github.com/uet-class/uet-class-backend/config"
 )
 
 var rdb *redis.Client
 var ctx context.Context
 
 func InitRedis() {
-	redisConfig := config.GetConfig()
 	ctx = context.Background()
 
+	databaseId, _ := strconv.Atoi(os.Getenv("REDIS_DATABASE"))
 	rdb = redis.NewClient(&redis.Options{
-		Addr:     fmt.Sprintf("%s:%s", redisConfig.GetString("REDIS_HOST"), redisConfig.GetString("REDIS_PORT")),
-		Password: redisConfig.GetString("REDIS_PASSWORD"),
-		DB:       redisConfig.GetInt("REDIS_DATABASE"),
+		Addr:     fmt.Sprintf("%s:%s", os.Getenv("REDIS_HOST"), os.Getenv("REDIS_PORT")),
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DB:       databaseId,
 	})
 }
 
