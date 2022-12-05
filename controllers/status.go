f950ad898abad26057640445a9fe6e26c75ae88a
thainm <thainm.uet@gmail.com>
2022-12-05 21:27:54 +0700

    Update status logger

--- controllers/status.go
+++ controllers/status.go
@@ -1,13 +1,17 @@
 package controllers
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
 
 func AbortWithError(c *gin.Context, code int, message interface{}) {
+	fmt.Println(message)
 	c.AbortWithStatusJSON(code, gin.H{"error": message})
 }
 
 func ResponseHandler(c *gin.Context, code int, message interface{}) {
+	fmt.Println(message)
 	c.JSON(code, gin.H{"message": message})
 }
