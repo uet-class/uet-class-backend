7a7e611bac35019d967f060ec7c33cd24116895a
thainm <thainm.uet@gmail.com>
2022-12-06 22:38:16 +0700

    Update GCS authentication mechanism

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"github.com/uet-class/uet-class-backend/database"
+	"github.com/uet-class/uet-class-backend/gcs"
 	"github.com/uet-class/uet-class-backend/server"
-	"github.com/uet-class/uet-class-backend/storage"
 )
 
 func main() {
-	storage.InitStorageClient()
+	gcs.InitStorageClient()
 	database.InitPostgres()
 	database.InitRedis()
 	server.Init()
