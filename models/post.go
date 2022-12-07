e0f423425130bbbfe0b31a766c1a64969c8a78c9
thainm <thainm.uet@gmail.com>
2022-12-07 19:33:54 +0700

    Add CreatorName into Post model

--- models/post.go
+++ models/post.go
@@ -6,9 +6,10 @@ import (
 
 type Post struct {
 	gorm.Model
-	ClassID   int       `gorm:"not null"`
-	CreatorID int       `gorm:"not null"`
-	Title     string    `gorm:"not null"`
-	Content   string    `gorm:"not null"`
-	Comment   []Comment `gorm:"foreignKey:PostID"`
+	ClassID     int       `gorm:"not null"`
+	CreatorID   int       `gorm:"not null"`
+	CreatorName string    `gorm:"not null"`
+	Title       string    `gorm:"not null"`
+	Content     string    `gorm:"not null"`
+	Comment     []Comment `gorm:"foreignKey:PostID"`
 }
